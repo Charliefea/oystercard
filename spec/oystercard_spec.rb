efda6b0c06e63ddfb76706925560258f713c0610
Kynosaur <kye.bracey@gmail.com>
2017-05-15 16:46:32 +0100

    Add test for deducting balance from Oystercard

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,7 @@
 require 'oystercard'
 describe Oystercard do
   subject(:oystercard) { described_class.new }
+  let(:balance_limit) { Oystercard::BALANCE_LIMIT }
 
   it 'has a default balance of 0' do
     expect(oystercard.balance).to eq 0
@@ -11,8 +12,15 @@ describe Oystercard do
       expect { oystercard.top_up(5) }.to change { oystercard.balance }.from(0).to(5)
     end
     it 'should prevent balance from exceeding £90' do
-      oystercard.top_up(Oystercard::BALANCE_LIMIT)
-      expect { oystercard.top_up(1) }.to raise_error {"Your balance cannot exceed £#{Oystercard::BALANCE_LIMIT}"}
+      oystercard.top_up(balance_limit)
+      expect { oystercard.top_up(1) }.to raise_error {"Your balance cannot exceed £#{balance_limit}"}
+    end
+  end
+
+  describe '#deduct' do
+    it 'should be able to have balance deducted' do
+      oystercard.top_up(balance_limit)
+      expect { oystercard.deduct(5) }.to change { oystercard.balance }.from(balance_limit).to(balance_limit-5)
     end
   end
 
