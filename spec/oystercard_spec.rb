dfd10d4c29e1aaa82783cde4a3208a60ad26e6c5
charliefea <charlotte.feather@yahoo.com>
2017-05-15 15:59:03 +0100

    Create test for default start balance = 0, create #initialize and add attr_reader

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,8 @@
 require 'oystercard'
 describe Oystercard do
+  subject(:oystercard) { described_class.new }
 
+  it 'has a default balance of 0' do
+    expect(oystercard.balance).to eq 0
+  end
 end
