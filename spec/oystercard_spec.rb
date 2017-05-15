82d7818308ed8346ac7953f73f3154e03d6e88fe
charliefea <charlotte.feather@yahoo.com>
2017-05-15 16:23:23 +0100

    Add test for preventing balance from exceeding £90

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,6 +10,9 @@ describe Oystercard do
     it 'should be able to be topped up' do
       expect { oystercard.top_up(5) }.to change { oystercard.balance }.from(0).to(5)
     end
+    it 'should prevent balance from exceeding £90' do
+      expect { oystercard.top_up(91) }.to raise_error {"Your balance cannot exceed £90"}
+    end
   end
 
 end
