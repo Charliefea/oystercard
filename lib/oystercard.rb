d47db5da8b3021e74ccad30244293e1b406a19e4
charliefea <charlotte.feather@yahoo.com>
2017-05-15 16:29:18 +0100

    Use interpolation to our code DRY

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,12 @@
 class Oystercard
   attr_reader :balance
-
+  BALANCE_LIMIT = 90
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
-    raise "Your balance cannot exceed £90" if @balance + amount > 90
+    raise "Your balance cannot exceed £#{BALANCE_LIMIT}" if @balance + amount > BALANCE_LIMIT
     @balance += amount
   end
 
