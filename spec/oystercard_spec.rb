3611f6b73a2e06d3e1f71e0a4120386348092368
Kynosaur <kye.bracey@gmail.com>
2017-05-15 16:14:09 +0100

    Add test for top_up method to increase balance, and write method to increase @balance by amount given

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,11 @@ describe Oystercard do
   it 'has a default balance of 0' do
     expect(oystercard.balance).to eq 0
   end
+
+  describe '#top_up' do
+    it 'should be able to be topped up' do
+      expect { oystercard.top_up(5) }.to change { oystercard.balance }.from(0).to(5)
+    end
+  end
+
 end
